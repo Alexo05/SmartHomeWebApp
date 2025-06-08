42cf842dd2c8959f3fbcf9d26cc10ca56c73693a
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-06-08 10:42:15 +0100

    commit10

--- smart-home-web-app/src/app/components/table/table.ts
+++ smart-home-web-app/src/app/components/table/table.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.html',
   styleUrl: './table.scss'
 })
 export class Table {
-
+  devices = [
+  {
+    id: 1,
+    name: 'Zinzu Chan Lee',
+    port: 'Seoul',
+    status: 'On',
+    image: '/assets/lighton.png'
+  },
+  {
+    id: 2,
+    name: 'Jeet Saru',
+    port: 'Kathmandu',
+    status: 'Off',
+    image: '/assets/lighton.png'
+  }
+];
 }
--- smart-home-web-app/src/app/devices/devices.ts
+++ smart-home-web-app/src/app/devices/devices.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { Table } from '../components/table/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-devices',
-  imports: [Table],
+  imports: [CommonModule, Table],
   templateUrl: './devices.html',
   styleUrl: './devices.scss'
 })
 export class Devices {
+
+  deviceTypes = [
+  {
+    id: 1,
+    name: 'Air Conditioner',
+    imageUrl: 'assets/images/air-conditioner.png',
+    imageUrl1: 'assets/images/air-conditioner.png',
+    color : "#ff7c7c",
+  },
+  {
+    id: 2,
+    name: 'Smart Lighting',
+    imageUrl: 'assets/images/lightoff.png',
+    imageUrl1: 'assets/images/lighton.png',
+    color: "#ffdc6c",
+  },
+   {
+    id: 3,
+    name: 'RGB Lighting',
+    imageUrl: 'assets/images/rgblight.png',
+    imageUrl1: 'assets/images/rgblight.png',
+    color: "#a16cff",
+  }
+];
 
 }
